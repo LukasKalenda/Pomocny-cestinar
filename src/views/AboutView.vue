6862587628e2d68e5f4b0ddf456261bd399cabba
LukasKalenda <kalenda.lu.2020@skola.ssps.cz>
2023-10-25 20:29:19 +0200

    feat: created tailwindCSS

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Cookies />
   </div>
 </template>
+<script setup>
+import Cookies from '@/components/common/Cookies.vue'
 
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
