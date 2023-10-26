e5f79e5db42543b0f0ff3d6b1f8cacc75db33e6f
LukasKalenda <kalenda.lu.2020@skola.ssps.cz>
2023-10-26 11:35:26 +0200

    feat: router links

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,78 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import FAQView from '../views/FAQView.vue'
-import NotFoundView from '../views/NotFoundView.vue'
-import ArticlesView from '../views/ArticlesView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      path: "/about",
+      name: "about",
+      component: () => import("../views/AboutView.vue"),
     },
     {
-      path: '/faq',
-      name: 'faq',
-      component: () => import('../views/FAQView.vue')
+      path: "/faq",
+      name: "faq",
+      component: () => import("../views/FAQView.vue"),
     },
     {
-      path: '/articles',
-      name: 'articles',
-      component: () => import('../views/ArticlesView.vue')
+      path: "/articles",
+      name: "articles",
+      component: () => import("../views/ArticlesView.vue"),
     },
     {
-      path: '/sign-up',	
-      name: 'signup',
-      component: () => import('../views/SignUpView.vue')
+      path: "/sign-up",
+      name: "signup",
+      component: () => import("../views/SignUpView.vue"),
     },
     {
-      path: '/sign-in',
-      name: 'signin',
-      component: () => import('../views/SignInView.vue')
+      path: "/sign-in",
+      name: "signin",
+      component: () => import("../views/SignInView.vue"),
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: () => import("../views/ProfileView.vue"),
+      meta: { requiresAuth: true },
     },
     // 404
     {
-      path: '/:pathMatch(.*)*',
-      name: 'not-found',
-      component: () => import('../views/NotFoundView.vue')
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: () => import("../views/NotFoundView.vue"),
     },
+  ],
+});
 
-  ]
-})
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener();
+        resolve(user);
+      },
+      reject
+    );
+  });
+};
 
-export default router
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // Check if user is logged in
+    if (await getCurrentUser()) {
+      next();
+    } else {
+      alert("You must be logged in to see this page.");
+      next({ name: "signin" });
+    }
+  }
+});
+
+export default router;
