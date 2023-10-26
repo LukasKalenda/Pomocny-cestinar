9d08d13a0ba58654ea8f70e7fb12b28fa5827fde
LukasKalenda <kalenda.lu.2020@skola.ssps.cz>
2023-10-26 09:57:29 +0200

    feat: setup firebase reading

--- src/views/ArticlesView.vue
+++ src/views/ArticlesView.vue
@@ -1,13 +1,30 @@
 <script setup>
-import { db } from "@/firebase"
-const articles = useCollection(collection(db, 'articles'))
+import { onMounted, ref } from "vue";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase";
+const articles = ref();
+onMounted(async () => {
+  const querySnapshot = await getDocs(collection(db, "articles"));
+  let articlesArray = [];
+
+  querySnapshot.forEach((doc) => {
+    const article = {
+      id: doc.id,
+      title: doc.data().title,
+      content: doc.data().content,
+    };
+    articlesArray.push(article);
+  })
+  articles.value = articlesArray;
+});
 </script>
 
 <template>
-    <div>articles!</div>
+  <div>articles!</div>
   <ul>
     <li v-for="a in articles" :key="a.id">
-        <span>{{ a.title }}</span>
+      <span> {{ a.id }}</span>
+      <span>{{ a.title }}</span>
       <span>{{ a.content }}</span>
     </li>
   </ul>
