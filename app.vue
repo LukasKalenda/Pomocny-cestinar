a2100faaa94334a25e94c44dfe74386057b868d1
LukasKalenda <kalenda.lu.2020@skola.ssps.cz>
2023-11-08 08:19:01 +0100

    mutatuon

--- app.vue
+++ app.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
+    <Header />
     <RouterView />
+    <Footer />
   </div>
 </template>
 
 <script setup>
-
+import Header from "./components/common/Header.vue";
+import Footer from "./components/common/Footer.vue";
 </script>
