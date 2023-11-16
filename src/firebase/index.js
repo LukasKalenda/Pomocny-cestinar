be0ea379bd53c93044a6bae5a9e1b21bd1f85e8d
VIPTrollik <simon@prerovsky.cz>
2023-11-16 10:21:26 +0100

    update firebase config to new firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,20 +2,23 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnl1wO-V36YDFU28N0piFwnYBFrNDZ3U0",
-  authDomain: "cznet-deploy.firebaseapp.com",
-  projectId: "cznet-deploy",
-  storageBucket: "cznet-deploy.appspot.com",
-  messagingSenderId: "879637577551",
-  appId: "1:879637577551:web:c626cf402ed2d02d6e8f3a",
-  measurementId: "G-X31X3NX16S"
+  apiKey: "AIzaSyBCw-x9MqmJXRu1wDLbUPhRzK7g9Buyywc",
+  authDomain: "pomocnycestinar.firebaseapp.com",
+  projectId: "pomocnycestinar",
+  storageBucket: "pomocnycestinar.appspot.com",
+  messagingSenderId: "805387389889",
+  appId: "1:805387389889:web:64ea3b62f288d7cb4e3d40",
+  measurementId: "G-ZRZT7C5MBN"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
-export { db, analytics }
+export { db, analytics, auth }
